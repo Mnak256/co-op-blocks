6359006a9bb45b973e8ba8f3de2cd06b03cbf860
Mainak Dutta <30327317+Mnak256@users.noreply.github.com>
2021-08-28 00:15:05 +0530

    Add ui

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+
 const port = process.env.PORT || 8256
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.send({text: 'hello world'})
+app.get('/hello', (req, res) => {
+  res.send({ text: 'hello world' })
 })
 
 app.listen(port)
