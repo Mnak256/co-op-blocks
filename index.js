43fb896e1a998882e81bbe1030942ec397ec840a
Mainak Dutta <30327317+Mnak256@users.noreply.github.com>
2021-08-29 11:56:14 +0530

    add landing page

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
+const lobby = require('./lobby')
 const app = express()
 
 const port = process.env.PORT || 8256
 
 app.use(express.static('public'))
 
-app.get('/hello', (req, res) => {
-  res.send({ text: 'hello world' })
-})
+app.use('/lobby', lobby)
 
 app.listen(port)
