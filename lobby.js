eda4cd4d3844d8ac07d944ee962367dc3d143c3a
Mnak256 <30327317+Mnak256@users.noreply.github.com>
2022-07-17 00:30:07 +0530

    clear all

--- lobby.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-router.get('/', function (req, res, next) {
-  res.sendFile('public/game.html', { root: __dirname })
-})
-
-module.exports = router
